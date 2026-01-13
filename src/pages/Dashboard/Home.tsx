71fc101bce0e1b75d70b8ff37faf3c5fe2441a8e
nimurr <nimurnerob404@gmail.com>
2026-01-13 14:51:44 +0600

    `Removed unused import StatisticsChart`

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,6 +1,5 @@
 import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
-import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
-import StatisticsChart from "../../components/ecommerce/StatisticsChart";
+import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart"; 
 import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
 import RecentOrders from "../../components/ecommerce/RecentOrders";
 import DemographicCard from "../../components/ecommerce/DemographicCard";
